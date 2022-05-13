f4d85cea8bb9cf9f18d44b1a80f9558bf9db7a33
Éverton Toffanetto <evertondgn@hotmail.com>
2022-05-13 17:10:44 -0300

    refactor: remove folders storybook

--- src/templates/Default/storybook/stories.tsx
+++
@@ -1,6 +0,0 @@
-import { Story } from '@storybook/react'
-import TemplateDefault from '..'
-
-const Template: Story = args => <TemplateDefault {...args} />
-
-export const Default = Template.bind({})
--- src/templates/Example/storybook/stories.tsx
+++
@@ -1,6 +0,0 @@
-import { Story } from '@storybook/react'
-import Example from '..'
-
-const Template: Story = args => <Example {...args} />
-
-export const Default = Template.bind({})
