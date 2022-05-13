7eb257bcec00cfb5b5218ff4517ece1612a36a5d
Éverton Toffanetto <evertondgn@hotmail.com>
2022-05-13 17:20:25 -0300

    refactor: adjust css of stories from Input component

--- src/components/atoms/Input/storybook/stories.tsx
+++ src/components/atoms/Input/storybook/stories.tsx
@@ -2,6 +2,8 @@ import { Story } from '@storybook/react'
 import Input from '..'
 import { InputProps } from '../types'
 
-const Template: Story<InputProps> = args => <Input {...args} />
+const Template: Story<InputProps> = args => (
+  <Input {...args} style={{ margin: 0, minWidth: '28rem' }} />
+)
 
 export const Default = Template.bind({})
