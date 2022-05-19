496159bc9f7b3c89773a5de3a09d0fcd02c7024b
Éverton Toffanetto <evertondgn@hotmail.com>
2022-05-19 17:25:52 -0300

    refactor: move provides to file separate

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,13 @@
-import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import { GlobalStyles } from 'globalStyles'
-import { PersistGate } from 'redux-persist/integration/react'
 import Routes from 'router'
-import { persistor, store } from 'store/configStore'
-import * as C from 'components'
+import { MainProviders } from 'utils'
 
 const App = () => (
-  <C.ErrorBoundary>
-    <Provider store={store}>
-      <PersistGate loading={<h1>Loading...</h1>} persistor={persistor}>
-        <BrowserRouter>
-          <Routes />
-        </BrowserRouter>
-        <GlobalStyles />
-      </PersistGate>
-    </Provider>
-  </C.ErrorBoundary>
+  <MainProviders>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </MainProviders>
 )
 
 export default App
