9b8ec5c2de68bc040c2e4eaa5bebc3f51db5ad30
Éverton Toffanetto <evertondgn@hotmail.com>
2022-05-28 04:46:09 -0300

    chore: config jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   collectCoverage: true,
+  fakeTimers: {
+    enableGlobally: true
+  },
   testPathIgnorePatterns: ['/node_modules'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
@@ -11,8 +14,8 @@ module.exports = {
     'src/app.tsx',
     'src/pages/index.tsx',
     'src/components/index.tsx',
+    'src/components/templates/index.tsx',
     'src/hooks/index.ts',
-    'src/templates/index.ts',
     'utils/providers',
     'src/utils/index.tsx',
     'src/assets',
@@ -28,7 +31,7 @@ module.exports = {
     'stories.tsx',
     'types.ts'
   ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
   roots: ['<rootDir>/src'],
   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.tsx?$',
   moduleNameMapper: {
@@ -39,5 +42,13 @@ module.exports = {
       '@swc/jest',
       { jsc: { transform: { react: { runtime: 'automatic' } } } }
     ]
+  },
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: -10
+    }
   }
 }
