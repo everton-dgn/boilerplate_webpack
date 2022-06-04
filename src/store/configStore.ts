b8376eb4daa91bd30d1b56cdf913a976cb756aa7
Éverton Toffanetto <evertondgn@hotmail.com>
2022-06-04 20:24:29 -0300

    chore: disabled devtools in prod

--- src/store/configStore.ts
+++ src/store/configStore.ts
@@ -5,7 +5,8 @@ import { persistedReducer, serializableCheck } from './persistConfig'
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({ serializableCheck })
+    getDefaultMiddleware({ serializableCheck }),
+  devTools: process.env.NODE_ENV !== 'production'
 })
 
 export const persistor = persistStore(store)
