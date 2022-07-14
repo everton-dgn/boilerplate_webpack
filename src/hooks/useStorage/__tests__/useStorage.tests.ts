051f6b681e944ebe41f037f0e7067e6708393367
Éverton Toffanetto <evertondgn@hotmail.com>
2022-07-14 09:00:01 -0300

    fix: initial value overwrites preexisting values in storage

--- src/hooks/useStorage/__tests__/useStorage.tests.ts
+++ src/hooks/useStorage/__tests__/useStorage.tests.ts
@@ -3,16 +3,29 @@ import { renderHooksProvider } from 'tests/providers'
 import { useStorage } from '..'
 
 describe('[Hooks] useStorage', () => {
+  afterEach(() => {
+    localStorage.clear()
+    sessionStorage.clear()
+  })
+
+  it('should to return the existing value in the storage if it exists and not overwrite with initialValue', () => {
+    localStorage.setItem('keyExample', JSON.stringify('prevValue'))
+    const { result } = renderHooksProvider(() =>
+      useStorage({ key: 'keyExample', initialValue: 'initialValueExample' })
+    )
+    expect(result.current.getStorage).toBe('prevValue')
+  })
+
   it('should to return initialValue', () => {
     const { result } = renderHooksProvider(() =>
-      useStorage({ key: 'keyExample1', initialValue: 'initialValueExample' })
+      useStorage({ key: 'keyExample', initialValue: 'initialValueExample' })
     )
     expect(result.current.getStorage).toBe('initialValueExample')
   })
 
   it('should to return initialValue and after the value of storage', () => {
     const { result } = renderHooksProvider(() =>
-      useStorage({ key: 'keyExample2', initialValue: 'initialValueExample' })
+      useStorage({ key: 'keyExample', initialValue: 'initialValueExample' })
     )
     expect(result.current.getStorage).toBe('initialValueExample')
     act(() => {
@@ -23,19 +36,33 @@ describe('[Hooks] useStorage', () => {
 
   it('should to return undefined', () => {
     const { result } = renderHooksProvider(() =>
-      useStorage({ key: 'keyExample3' })
+      useStorage({ key: 'keyExample' })
     )
     expect(result.current.getStorage).toBeUndefined()
   })
 
-  it('should to save data', () => {
+  it('should to save data in localStorage', () => {
     const { result } = renderHooksProvider(() =>
-      useStorage({ key: 'keyExample4' })
+      useStorage({ key: 'keyExample' })
     )
     expect(result.current.getStorage).toBeUndefined()
     act(() => {
       result.current.setStorageState('valueExample')
     })
     expect(result.current.getStorage).toBe('valueExample')
+  })
+
+  it('should to save data in sessionStorage', () => {
+    const { result } = renderHooksProvider(() =>
+      useStorage({ key: 'keyExample', storage: 'sessionStorage' })
+    )
+    expect(result.current.getStorage).toBeUndefined()
+    act(() => {
+      result.current.setStorageState('valueExample')
+    })
+    expect(result.current.getStorage).toBe('valueExample')
+    expect(JSON.parse(sessionStorage.getItem('keyExample')!)).toBe(
+      'valueExample'
+    )
   })
 })
--- src/infra/storageAdapter/__tests__/storageAdapter.test.ts
+++ src/infra/storageAdapter/__tests__/storageAdapter.test.ts
@@ -1,73 +1,86 @@
 import { storageAdapter } from '..'
 
 describe('[infra] storageAdapter', () => {
+  afterEach(() => {
+    localStorage.clear()
+    sessionStorage.clear()
+  })
+
+  it('should to return the existing value in the storage if it exists and not overwrite with initialValue', () => {
+    localStorage.setItem('keyExample', JSON.stringify('prevValue'))
+    const { getStorageAdapter } = storageAdapter()
+    expect(getStorageAdapter('keyExample', 'initialValueExample')).toBe(
+      'prevValue'
+    )
+  })
+
   it('should return initialValue', () => {
     const { getStorageAdapter } = storageAdapter()
-    expect(getStorageAdapter('keyExample1', 'initialValueExample')).toBe(
+    expect(getStorageAdapter('keyExample', 'initialValueExample')).toBe(
       'initialValueExample'
     )
   })
 
   it('should return a number', () => {
     const { getStorageAdapter, setStorageAdapter } = storageAdapter()
-    setStorageAdapter('keyExample2', 22)
-    expect(getStorageAdapter('keyExample2')).toBe(22)
+    setStorageAdapter('keyExample', 22)
+    expect(getStorageAdapter('keyExample')).toBe(22)
   })
 
   it('should return a string', () => {
     const { getStorageAdapter, setStorageAdapter } = storageAdapter()
-    setStorageAdapter('keyExample3', 'example')
-    expect(getStorageAdapter('keyExample3')).toBe('example')
+    setStorageAdapter('keyExample', 'example')
+    expect(getStorageAdapter('keyExample')).toBe('example')
   })
 
   it('should return undefined', () => {
     const { getStorageAdapter, setStorageAdapter } = storageAdapter()
-    setStorageAdapter('keyExample4', undefined)
-    expect(getStorageAdapter('keyExample4')).toBeUndefined()
-    setStorageAdapter('keyExample5', null)
-    expect(getStorageAdapter('keyExample5')).toBeUndefined()
+    setStorageAdapter('keyExample', undefined)
+    expect(getStorageAdapter('keyExample')).toBeUndefined()
+    setStorageAdapter('keyExample2', null)
+    expect(getStorageAdapter('keyExample2')).toBeUndefined()
   })
 
   it('should return a array', () => {
     const { getStorageAdapter, setStorageAdapter } = storageAdapter()
-    setStorageAdapter('keyExample6', [])
-    expect(getStorageAdapter('keyExample6')).toStrictEqual([])
+    setStorageAdapter('keyExample', [])
+    expect(getStorageAdapter('keyExample')).toStrictEqual([])
   })
 
   it('should return a object', () => {
     const { getStorageAdapter, setStorageAdapter } = storageAdapter()
-    setStorageAdapter('keyExample7', {})
-    expect(getStorageAdapter('keyExample7')).toStrictEqual({})
+    setStorageAdapter('keyExample', {})
+    expect(getStorageAdapter('keyExample')).toStrictEqual({})
   })
 
   it('should to record and return a value of the sessionStorage', () => {
     const { getStorageAdapter, setStorageAdapter } =
       storageAdapter('sessionStorage')
-    setStorageAdapter('keyExample8', 555)
-    expect(getStorageAdapter('keyExample8')).toBe(555)
+    setStorageAdapter('keyExample', 555)
+    expect(getStorageAdapter('keyExample')).toBe(555)
   })
 
   it('should to remove a item of the storage', () => {
     const { getStorageAdapter, setStorageAdapter, removeItemOfStorageAdapter } =
       storageAdapter()
-    setStorageAdapter('keyExample9', 'example')
-    setStorageAdapter('keyExample10', 'example2')
-    expect(getStorageAdapter('keyExample9')).toBe('example')
-    expect(getStorageAdapter('keyExample10')).toBe('example2')
-    removeItemOfStorageAdapter('keyExample9')
-    expect(getStorageAdapter('keyExample9')).toBeUndefined()
-    expect(getStorageAdapter('keyExample10')).toBe('example2')
+    setStorageAdapter('keyExample', 'example')
+    setStorageAdapter('keyExample2', 'example2')
+    expect(getStorageAdapter('keyExample')).toBe('example')
+    expect(getStorageAdapter('keyExample2')).toBe('example2')
+    removeItemOfStorageAdapter('keyExample')
+    expect(getStorageAdapter('keyExample')).toBeUndefined()
+    expect(getStorageAdapter('keyExample2')).toBe('example2')
   })
 
   it('should to remove all items of the storage', () => {
     const { getStorageAdapter, setStorageAdapter, clearStorageAdapter } =
       storageAdapter()
-    setStorageAdapter('keyExample11', 'example')
-    setStorageAdapter('keyExample12', 'example2')
-    expect(getStorageAdapter('keyExample11')).toBe('example')
-    expect(getStorageAdapter('keyExample12')).toBe('example2')
+    setStorageAdapter('keyExample', 'example')
+    setStorageAdapter('keyExample2', 'example2')
+    expect(getStorageAdapter('keyExample')).toBe('example')
+    expect(getStorageAdapter('keyExample2')).toBe('example2')
     clearStorageAdapter()
-    expect(getStorageAdapter('keyExample11')).toBeUndefined()
-    expect(getStorageAdapter('keyExample12')).toBeUndefined()
+    expect(getStorageAdapter('keyExample')).toBeUndefined()
+    expect(getStorageAdapter('keyExample2')).toBeUndefined()
   })
 })
