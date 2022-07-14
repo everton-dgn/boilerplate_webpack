f184a273f43f8298e29f628c9f1cb15de7ae21d7
Éverton Toffanetto <evertondgn@hotmail.com>
2022-07-14 00:17:23 -0300

    feat(infra): add storageAdapter

---
+++ src/infra/storageAdapter/index.ts
@@ -0,0 +1,37 @@
+export const storageAdapter = (storage?: 'sessionStorage') => {
+  const isClientBrowser = typeof window === 'object'
+  const setStorageLocation = () => {
+    return isClientBrowser && storage === 'sessionStorage'
+      ? sessionStorage
+      : localStorage
+  }
+
+  const getStorageAdapter = <T>(
+    key: string,
+    initialValue?: T
+  ): T | undefined => {
+    const hasStorage = setStorageLocation().getItem(key)
+    const hasValueValid = () => {
+      if (!hasStorage) return
+      return hasStorage === 'undefined' ? undefined : JSON.parse(hasStorage)
+    }
+    return (hasValueValid() as T) ?? initialValue
+  }
+
+  const setStorageAdapter = (key: string, value: any): void => {
+    setStorageLocation().setItem(key, JSON.stringify(value))
+  }
+
+  const removeItemOfStorageAdapter = (key: string): void => {
+    setStorageLocation().removeItem(key)
+  }
+
+  const clearStorageAdapter = (): void => setStorageLocation().clear()
+
+  return {
+    getStorageAdapter,
+    setStorageAdapter,
+    removeItemOfStorageAdapter,
+    clearStorageAdapter
+  }
+}
