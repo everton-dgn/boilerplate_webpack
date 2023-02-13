32f1255cff40d649bda578636de170430e11bfee
Éverton Toffanetto <evertondgn@hotmail.com>
2023-02-13 00:33:20 -0300

    test: fix tests

--- src/ui/pages/Home/__tests__/Home.test.tsx
+++ src/ui/pages/Home/__tests__/Home.test.tsx
@@ -11,74 +11,14 @@ describe('[Page] Home', () => {
     mockedUseNavigate.mockClear()
   })
 
-  it('should alternate the text displayed in two h1 each time the button is clicked', async () => {
+  it(`should go to another page by clicking the button 1 and it should preload the next page with the onMouseEnter event`, async () => {
     renderWithProviders(<Home />)
-
-    const btn = screen.getByRole('button', { name: 'Change Global State' })
-    const titleSectionStateExample1 = screen.getByRole('heading', {
-      name: 'Redux Example State: false'
+    const btn = screen.getByRole('button', {
+      name: 'Page Example 1'
     })
-    const titleSectionPayload1 = screen.getByRole('heading', {
-      name: 'Payload: Empty'
-    })
-
-    expect(titleSectionStateExample1).toBeInTheDocument()
-    expect(titleSectionPayload1).toBeInTheDocument()
-
     await user.click(btn)
-    const titleSectionStateExample2 = screen.getByRole('heading', {
-      name: 'Redux Example State: true'
-    })
-    const titleSectionPayload2 = screen.getByRole('heading', {
-      name: 'Payload: Example Text'
-    })
-
-    expect(titleSectionStateExample2).toBeInTheDocument()
-    expect(titleSectionPayload2).toBeInTheDocument()
-
-    await user.click(btn)
-    const titleSectionStateExample3 = screen.getByRole('heading', {
-      name: 'Redux Example State: false'
-    })
-    const titleSectionPayload3 = screen.getByRole('heading', {
-      name: 'Payload: Empty'
-    })
-
-    expect(titleSectionStateExample3).toBeInTheDocument()
-    expect(titleSectionPayload3).toBeInTheDocument()
-  })
-
-  const dataTests = [
-    { buttonNumber: 1 },
-    { buttonNumber: 2 },
-    { buttonNumber: 3 }
-  ]
-  dataTests.forEach(({ buttonNumber }) => {
-    it(`should go to another page by clicking the button ${buttonNumber} and it should preload the next page with the onMouseEnter event`, async () => {
-      renderWithProviders(<Home />)
-
-      const btn = screen.getByRole('button', {
-        name: `Page Example ${buttonNumber}`
-      })
-      await user.click(btn)
-      await user.hover(btn)
-
-      expect(mockedUseNavigate).toHaveBeenCalledTimes(1)
-      expect(mockedUseNavigate).toHaveBeenCalledWith(
-        `page-example-${buttonNumber}`
-      )
-    })
-  })
-
-  it('should toggle button text and data-theme value that tag body when clicking change theme button', async () => {
-    renderWithProviders(<Home />)
-
-    const btnTheme = screen.getByRole('button', { name: /theme/i })
-
-    expect(btnTheme).toHaveTextContent('Theme light')
-    expect(document.body.dataset.theme).toBe('light')
-    await user.click(btnTheme)
-    expect(btnTheme).toHaveTextContent('Theme dark')
-    expect(document.body.dataset.theme).toBe('dark')
+    await user.hover(btn)
+    expect(mockedUseNavigate).toHaveBeenCalledTimes(1)
+    expect(mockedUseNavigate).toHaveBeenCalledWith('page-example-1')
   })
 })
