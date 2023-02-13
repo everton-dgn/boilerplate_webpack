302b75a96f5465e9c6ef1c517138e33da2c872f3
Éverton Toffanetto <evertondgn@hotmail.com>
2023-02-12 23:45:58 -0300

    chore: update templates plop

--- componentGenerator/plopfile.js
+++ componentGenerator/plopfile.js
@@ -1,92 +1,65 @@
-const components = ['atoms', 'molecules', 'organisms']
-const template = `../src/ui/components/templates/{{pascalCase name}}`
-const type = 'add'
-const page = `../src/ui/pages/{{pascalCase name}}`
-const prompts = [
+const atomicDesignLayers = [
   {
-    type: 'input',
-    name: 'name',
-    message: 'What is your component name?'
+    name: 'Atom',
+    value: 'atoms'
+  },
+  {
+    name: 'Molecule',
+    value: 'molecules'
+  },
+  {
+    name: 'Organism',
+    value: 'organisms'
+  },
+  {
+    name: 'Template',
+    value: 'templates'
   }
 ]
 
 module.exports = plop => {
-  components.map(el => {
-    const component = `../src/ui/components/${el}/{{pascalCase name}}`
+  const component = `../src/ui/components/{{component_level}}/{{pascalCase name}}`
 
-    return plop.setGenerator(el, {
-      description: `Create a ${el}`,
-      prompts,
-      actions: [
-        {
-          type,
-          path: `${component}/index.tsx`,
-          templateFile: 'templates/component.tsx.hbs'
-        },
-        {
-          type,
-          path: `${component}/styles.ts`,
-          templateFile: 'templates/styles.ts.hbs'
-        },
-        {
-          type,
-          path: `${component}/storybook/stories.tsx`,
-          templateFile: 'templates/stories.tsx.hbs'
-        },
-        {
-          type,
-          path: `${component}/__tests__/test.tsx`,
-          templateFile: 'templates/test.tsx.hbs'
-        },
-        {
-          type,
-          path: `${component}/types.ts`,
-          templateFile: 'templates/types.ts.hbs'
-        }
-      ]
-    })
-  })
-
-  plop.setGenerator('templates', {
-    description: `Create a templates`,
-    prompts,
-    actions: [
+  return plop.setGenerator('components', {
+    description: `Create a Component`,
+    prompts: [
       {
-        type,
-        path: `${template}/index.tsx`,
-        templateFile: 'templates/template.tsx.hbs'
+        type: 'input',
+        name: 'name',
+        message: 'Name of your component?'
       },
       {
-        type,
-        path: `${template}/styles.ts`,
-        templateFile: 'templates/styles.ts.hbs'
-      },
-      {
-        type,
-        path: `${template}/__tests__/test.tsx`,
-        templateFile: 'templates/test.tsx.hbs'
+        type: 'list',
+        name: 'component_level',
+        message: 'What is the level of your component?',
+        choices: atomicDesignLayers
       }
-    ]
-  })
-
-  plop.setGenerator('pages', {
-    description: `Create a pages`,
-    prompts,
+    ],
     actions: [
       {
-        type,
-        path: `${page}/index.tsx`,
-        templateFile: 'templates/page.tsx.hbs'
+        type: 'add',
+        path: `${component}/index.tsx`,
+        templateFile: 'templates/component.tsx.hbs'
       },
       {
-        type,
-        path: `${page}/styles.ts`,
+        type: 'add',
+        path: `${component}/styles.ts`,
         templateFile: 'templates/styles.ts.hbs'
       },
       {
-        type,
-        path: `${page}/__tests__/test.tsx`,
+        type: 'add',
+        path: `${component}/storybook/stories.tsx`,
+        templateFile: 'templates/stories.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: `${component}/__tests__/test.tsx`,
         templateFile: 'templates/test.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: `${component}/types.ts`,
+        templateFile: 'templates/types.ts.hbs'
       }
     ]
   })
